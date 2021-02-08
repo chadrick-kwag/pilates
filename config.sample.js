07c1bd1c4d000c696589174a61d38269557c6280
chadrick-kwag <chadrick.kwag@gmail.com>
2021-02-08 21:11:14 +0900

    update sample config

--- config.sample.js
+++ config.sample.js
@@ -1,8 +1,9 @@
 const address = 'localhost'
-const GRAPHQL_PORT = 4000
+const GRAPHQL_PORT_INTERNAL = 4000
+const GRAPHQL_PORT_EXTERNAL = 4000
 const MAIN_SERVE_PORT = 8080
 
-const gql_server_addr = `http://${address}:${GRAPHQL_PORT}`
+const gql_server_addr = `http://${address}:${GRAPHQL_PORT_EXTERNAL}`
 
 const postgres_access_info = {
     user: 'postgres',
@@ -15,12 +16,12 @@ const postgres_access_info = {
 
 const graphql_server_options = {
     host: '0.0.0.0',
-    port: GRAPHQL_PORT
+    port: GRAPHQL_PORT_INTERNAL
 }
 
 module.exports = {
     gql_server_addr, postgres_access_info, graphql_server_options, MAIN_SERVE_PORT,
-    GRAPHQL_PORT,
+    GRAPHQL_PORT_INTERNAL, GRAPHQL_PORT_EXTERNAL,
     address
 
 }
