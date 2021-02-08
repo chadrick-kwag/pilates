ec2009805fad6ef378cc1451da208cfca1109c47
chadrick-kwag <chadrick.kwag@gmail.com>
2021-02-08 20:01:35 +0900

    upate config sample js

--- config.sample.js
+++ config.sample.js
@@ -1,19 +1,26 @@
-const gql_server_addr = "http://mysite:4000/"
+const address = 'localhost'
+const GRAPHQL_PORT = 4000
+const MAIN_SERVE_PORT = 8080
+
+const gql_server_addr = `http://${address}:${GRAPHQL_PORT}`
 
 const postgres_access_info = {
-    user: 'someuser',
+    user: 'postgres',
     host: 'localhost',
-    database: 'somedb',
-    password: 'somepw',
+    database: 'test',
+    password: 'rootpw',
     port: 5432,
 }
 
 
 const graphql_server_options = {
     host: '0.0.0.0',
-    port: 4000
+    port: GRAPHQL_PORT
 }
 
 module.exports = {
-    gql_server_addr, postgres_access_info, graphql_server_options
+    gql_server_addr, postgres_access_info, graphql_server_options, MAIN_SERVE_PORT,
+    GRAPHQL_PORT,
+    address
+
 }
