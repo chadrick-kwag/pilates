ac61106c0fd3acc49c5b0174be9f5165c91f5d6c
chadrick-kwag <chadrick.kwag@gmail.com>
2021-02-08 21:52:48 +0900

    add version span in top nav bar

--- src/TopNavBar.js
+++ src/TopNavBar.js
@@ -1,7 +1,7 @@
 import React from 'react'
-import { Button } from 'react-bootstrap'
+import { OverlayTrigger, Tooltip } from 'react-bootstrap'
 import './TopNavBar.css'
-
+import packagejson from '../package.json'
 
 class TopNavBar extends React.Component {
 
@@ -28,31 +28,39 @@ class TopNavBar extends React.Component {
 
     render() {
 
+        console.log(packagejson)
+
         return <div className="bar-container">
-            <div className={this.get_bar_item_classname('client')} onClick={e => {
-                this.setState({
-                    selected: 'client'
-                })
-                this.props.onClientManageClick()
-            }}>회원관리</div>
-            <div className={this.get_bar_item_classname('instructor')} onClick={e => {
-                this.setState({
-                    selected: 'instructor'
-                })
-                this.props.onInstructorManageClick()
-            }}>강사관리</div>
-            <div className={this.get_bar_item_classname('plan')} onClick={e => {
-                this.setState({
-                    selected: 'plan'
-                })
-                this.props.onPlanManageClick()
-            }}>플랜관리</div>
-            <div className={this.get_bar_item_classname('schedule')} onClick={e => {
-                this.setState({
-                    selected: 'schedule'
-                })
-                this.props.onScheduleManageClick()
-            }}>스케쥴관리</div>
+            <div className="row-gravity-left">
+                <div className={this.get_bar_item_classname('client')} onClick={e => {
+                    this.setState({
+                        selected: 'client'
+                    })
+                    this.props.onClientManageClick()
+                }}>회원관리</div>
+                <div className={this.get_bar_item_classname('instructor')} onClick={e => {
+                    this.setState({
+                        selected: 'instructor'
+                    })
+                    this.props.onInstructorManageClick()
+                }}>강사관리</div>
+                <div className={this.get_bar_item_classname('plan')} onClick={e => {
+                    this.setState({
+                        selected: 'plan'
+                    })
+                    this.props.onPlanManageClick()
+                }}>플랜관리</div>
+                <div className={this.get_bar_item_classname('schedule')} onClick={e => {
+                    this.setState({
+                        selected: 'schedule'
+                    })
+                    this.props.onScheduleManageClick()
+                }}>스케쥴관리</div>
+            </div>
+            <div>
+                <span className='version' style={{fontSize: '0.8rem', color: 'white'}}>ver {packagejson.version}</span>
+            </div>
+
         </div>
     }
 }
