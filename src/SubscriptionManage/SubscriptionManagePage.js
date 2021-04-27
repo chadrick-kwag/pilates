3140ba817aae64bf722490e76aa817fffce46846
chadrick-kwag <chadrick.kwag@gmail.com>
2021-04-27 17:40:07 +0900

    new create plan view

--- src/SubscriptionManage/SubscriptionManagePage.js
+++ src/SubscriptionManage/SubscriptionManagePage.js
@@ -1,40 +1,44 @@
 import React from 'react'
 
 
-import {Button} from 'react-bootstrap'
+import { Button } from 'react-bootstrap'
 
 import SubscriptionListView from './SubscriptionListView'
 import CreateSubscriptionView from './CreateSubscriptionView'
 
+import CreatePlanView from './CreatePlanView/Base'
 
 
-class SubscriptionManagePage extends React.Component{
 
-    constructor(props){
+class SubscriptionManagePage extends React.Component {
+
+    constructor(props) {
         super(props)
 
-        this.state={
+        this.state = {
             viewmode: 'list'
         }
     }
 
 
-    render(){
+    render() {
         let mainview
-        if(this.state.viewmode=='list'){
-            mainview = <SubscriptionListView apolloclient={this.props.apolloclient}/>
+        if (this.state.viewmode == 'list') {
+            mainview = <SubscriptionListView apolloclient={this.props.apolloclient} />
         }
-        else if(this.state.viewmode =='create'){
-            mainview = <CreateSubscriptionView onCancelClick={()=>this.setState({viewmode: 'list'})}
-            onSubmitSuccess={()=>this.setState({viewmode: 'list'})}
-            apolloclient={this.props.apolloclient}
-            />
+        else if (this.state.viewmode == 'create') {
+            // mainview = <CreateSubscriptionView onCancelClick={()=>this.setState({viewmode: 'list'})}
+            // onSubmitSuccess={()=>this.setState({viewmode: 'list'})}
+            // apolloclient={this.props.apolloclient}
+            // />
+
+            mainview = <CreatePlanView onCancel={() => this.setState({ viewmode: 'list' })} />
         }
 
 
         return <div>
             <div>
-                <Button onClick={e=>this.setState({
+                <Button onClick={e => this.setState({
                     viewmode: 'create'
                 })}>플랜생성</Button>
             </div>
