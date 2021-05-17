b7edf791c7625eda934ff2986369175a7be4bdd5
chadrick-kwag <chadrick.kwag@gmail.com>
2021-05-17 22:20:44 +0900

    change guideline plan costs

--- src/SubscriptionManage/PredefinedPlans.js
+++ src/SubscriptionManage/PredefinedPlans.js
@@ -1,93 +1,95 @@
+
+
 const PREDEFINED_PLANS = {
     'PILATES': {
         'INDIVIDUAL': [
             {
                 rounds: 1,
                 cost: 99000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
 
             }, {
                 rounds: 10,
                 cost: 880000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
 
             },
             {
                 rounds: 21,
                 cost: 1760000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
             },
             {
                 rounds: 33,
                 cost: 2640000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '9개월',
+                expire_days: 270
             }
         ],
         'SEMI': [
             {
                 rounds: 1,
                 cost: 60000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 10,
                 cost: 500000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 21,
                 cost: 1000000,
-                expire_countdown: '3개월',
-                expire_days: 30
+                expire_countdown: '6개월',
+                expire_days: 180
             },
             {
                 rounds: 33,
                 cost: 1500000,
-                expire_countdown: '3개월',
-                expire_days: 30
+                expire_countdown: '9개월',
+                expire_days: 270
             }
         ],
         'GROUP': [
             {
                 rounds: 1,
                 cost: 40000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 4,
                 cost: 120000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 8,
                 cost: 230000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 12,
                 cost: 320000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
             },
             {
                 rounds: 24,
                 cost: 620000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '9개월',
+                expire_days: 270
             }, {
                 rounds: 36,
                 cost: 864000,
                 expire_countdown: '3개월',
-                expire_days: 90
+                expire_days: 270
             }
         ]
     },
@@ -97,90 +99,90 @@ const PREDEFINED_PLANS = {
             {
                 rounds: 1,
                 cost: 99000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
 
             }, {
                 rounds: 10,
                 cost: 88000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
 
             },
             {
                 rounds: 21,
                 cost: 1760000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
             },
             {
                 rounds: 33,
                 cost: 2640000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '9개월',
+                expire_days: 270
             }
         ],
         'SEMI': [
             {
                 rounds: 1,
                 cost: 60000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 10,
                 cost: 500000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             }, {
                 rounds: 21,
                 cost: 1000000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
             },
             {
                 rounds: 33,
                 cost: 1500000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '9개월',
+                expire_days: 270
             }
         ],
         'GROUP': [
             {
                 rounds: 1,
                 cost: 40000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 4,
                 cost: 140000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 8,
                 cost: 270000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 12,
                 cost: 380000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
             },
             {
                 rounds: 24,
                 cost: 729000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '9개월',
+                expire_days: 270
             },
             {
                 rounds: 36,
                 cost: 1026000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '9개월',
+                expire_days: 270
             }
         ]
     },
@@ -189,21 +191,21 @@ const PREDEFINED_PLANS = {
             {
                 rounds: 1,
                 cost: 65000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
 
             }, {
                 rounds: 5,
                 cost: 270000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
 
             },
             {
                 rounds: 10,
                 cost: 480000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             }
 
         ],
@@ -211,19 +213,19 @@ const PREDEFINED_PLANS = {
             {
                 rounds: 1,
                 cost: 40000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 5,
                 cost: 180000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             }, {
                 rounds: 10,
                 cost: 300000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             }
 
         ],
@@ -231,46 +233,46 @@ const PREDEFINED_PLANS = {
             {
                 rounds: 1,
                 cost: 30000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
 
             },
             {
                 rounds: 4,
                 cost: 100000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 8,
                 cost: 160000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 12,
                 cost: 210000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
             },
             {
                 rounds: 16,
                 cost: 240000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
 
             },
             {
                 rounds: 20,
                 cost: 270000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
             },
             {
                 rounds: 40,
                 cost: 450000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '9개월',
+                expire_days: 270
             }
         ]
     },
@@ -279,21 +281,21 @@ const PREDEFINED_PLANS = {
             {
                 rounds: 1,
                 cost: 65000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
 
             }, {
                 rounds: 5,
                 cost: 270000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
 
             },
             {
                 rounds: 10,
                 cost: 480000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             }
 
         ],
@@ -301,19 +303,19 @@ const PREDEFINED_PLANS = {
             {
                 rounds: 1,
                 cost: 40000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 5,
                 cost: 180000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             }, {
                 rounds: 10,
                 cost: 300000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             }
 
         ],
@@ -321,46 +323,46 @@ const PREDEFINED_PLANS = {
             {
                 rounds: 1,
                 cost: 30000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
 
             },
             {
                 rounds: 4,
                 cost: 100000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 8,
                 cost: 160000,
-                expire_countdown: '1개월',
-                expire_days: 30
+                expire_countdown: '3개월',
+                expire_days: 90
             },
             {
                 rounds: 12,
                 cost: 210000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
             },
             {
                 rounds: 16,
                 cost: 240000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
 
             },
             {
                 rounds: 20,
                 cost: 270000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '6개월',
+                expire_days: 180
             },
             {
                 rounds: 40,
                 cost: 450000,
-                expire_countdown: '3개월',
-                expire_days: 90
+                expire_countdown: '9개월',
+                expire_days: 270
             }
         ]
     }
