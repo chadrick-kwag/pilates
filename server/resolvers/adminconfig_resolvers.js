633ab47b586a696ef5de3ab93c36789778af5d09
chadrick-kwag <chadrick.kwag@gmail.com>
2021-07-27 15:28:18 +0900

    add auth check for checkin config resolvers

--- server/resolvers/adminconfig_resolvers.js
+++ server/resolvers/adminconfig_resolvers.js
@@ -1,10 +1,20 @@
 const pgclient = require('../pgclient')
 const { setting, save_setting, update_setting } = require('../adminappconfig')
+const { ensure_admin_account_id_in_context } = require('./common')
 
 
 module.exports = {
     Query: {
         fetch_checkin_configs: async (parent, args, context) => {
+
+            
+            if (!ensure_admin_account_id_in_context(context)) {
+                return {
+                    success: false,
+                    msg: 'invalid token'
+                }
+            }
+
             try {
 
                 console.log(setting)
@@ -26,6 +36,14 @@ module.exports = {
     },
     Mutation: {
         update_checkin_configs: async (parent, args, context) => {
+
+            
+            if (!ensure_admin_account_id_in_context(context)) {
+                return {
+                    success: false,
+                    msg: 'invalid token'
+                }
+            }
 
             try {
 
