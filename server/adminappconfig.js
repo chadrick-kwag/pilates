b9a5fd2c95877162bee5d7fdc43f4c479da83e39
chadrick-kwag <chadrick.kwag@gmail.com>
2021-07-28 09:54:02 +0900

    add fallback sample yaml file for admin app config and use this when no admin app config file exists

--- server/adminappconfig.js
+++ server/adminappconfig.js
@@ -1,13 +1,31 @@
 const fs = require('fs')
 const path = require('path')
-const filepath = path.join(__dirname, '../admin_web_config.yaml')
-console.log(filepath)
 const yaml = require('yaml')
 
-const file = fs.readFileSync(filepath, 'utf8')
+// check if file exists
+const filepath = path.join(__dirname, '../admin_web_config.yaml')
+let filecontent
+try{
+    if(!fs.existsSync(filepath)){
+        // if not exist, then read from sample file
+        const sample_filepath = path.join(__dirname, '../admin_app_config.sample.yaml')
+        console.log(`${filepath} not exist. reading from ${sample_filepath}`)
+        filecontent = fs.readFileSync(sample_filepath, 'utf8')
+    
+    }
+    else{
+        filecontent = fs.readFileSync(filepath, 'utf8')
+    }
+}catch(e){
+    console.log('error while reading from admin web config file. abort')
+    console.log(e)
+    process.exit(1)
+}
 
-const setting = yaml.parse(file)
-console.log(setting)
+
+
+const setting = yaml.parse(filecontent)
+
 const save_setting = () => {
     fs.writeFileSync(filepath, yaml.stringify(setting))
 }
@@ -29,11 +47,6 @@ const update_setting = (new_config, part) => {
     }
 
     setting[part] = sub_part
-
-    console.log('after update_setting')
-
-    console.log(setting)
-
     save_setting()
 
     return true
