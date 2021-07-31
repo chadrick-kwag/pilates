0601a220152871093eac7a22b0ec26e5902aca6c
chadrick-kwag <chadrick.kwag@gmail.com>
2021-07-31 16:07:27 +0900

    add terser plugin to remove console logs in production mode for all app webpack config

--- webpack.checkinapp.config.js
+++ webpack.checkinapp.config.js
@@ -1,54 +1,112 @@
 const hwp = require('html-webpack-plugin')
 const path = require('path')
-const {GRAPHQL_PORT_INTERNAL} = require('./config.js')
+const { GRAPHQL_PORT_INTERNAL } = require('./config.js')
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-    entry: './checkin_app_src/app.js',
-    output: {
-        path: path.resolve(__dirname, 'checkin_app_dist'),
-        filename: 'bundle.js'
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                include: path.resolve(__dirname, 'checkin_app_src'),
-                exclude: "/node_modules",
-                use: "babel-loader"
+
+module.exports = (env, argv) => {
+    if (argv.mode === 'development') {
+        return {
+            entry: './checkin_app_src/app.js',
+            output: {
+                path: path.resolve(__dirname, 'checkin_app_dist'),
+                filename: 'bundle.js'
             },
-            {
-                test: /\.css/,
-                use: ["style-loader", "css-loader"]
-            }, {
-                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot)$/,
-                use: 'file-loader?name=assets/[name].[hash].[ext]'
+            module: {
+                rules: [
+                    {
+                        test: /\.jsx?$/,
+                        include: path.resolve(__dirname, 'checkin_app_src'),
+                        exclude: "/node_modules",
+                        use: "babel-loader"
+                    },
+                    {
+                        test: /\.css/,
+                        use: ["style-loader", "css-loader"]
+                    }, {
+                        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot)$/,
+                        use: 'file-loader?name=assets/[name].[hash].[ext]'
+                    }
+                ]
+            },
+            plugins: [
+                new hwp({
+                    template: "./checkin_app_src/template.html",
+                    publicPath: (() => {
+
+                        console.log(process.env.NODE_ENV)
+
+                        if (process.env.NODE_ENV === 'WEBPACK_DEV_SERVER') {
+                            return '/'
+                        }
+                        else {
+                            return "/checkin"
+                        }
+                    })()
+                })
+
+            ],
+            devServer: {
+                open: "chrome",
+                host: 'localhost',
+                port: 9001,
+                compress: false,
+                proxy: {
+                    '/graphql': `http://localhost:${GRAPHQL_PORT_INTERNAL}`
+                }
             }
-        ]
-    },
-    plugins: [
-        new hwp({
-            template: "./checkin_app_src/template.html",
-            publicPath: (()=>{
-                
-                console.log(process.env.NODE_ENV)
+        }
+    }
+    else {
+        return {
+            entry: './checkin_app_src/app.js',
+            output: {
+                path: path.resolve(__dirname, 'checkin_app_dist'),
+                filename: 'bundle.js'
+            },
+            module: {
+                rules: [
+                    {
+                        test: /\.jsx?$/,
+                        include: path.resolve(__dirname, 'checkin_app_src'),
+                        exclude: "/node_modules",
+                        use: "babel-loader"
+                    },
+                    {
+                        test: /\.css/,
+                        use: ["style-loader", "css-loader"]
+                    }, {
+                        test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot)$/,
+                        use: 'file-loader?name=assets/[name].[hash].[ext]'
+                    }
+                ]
+            },
+            plugins: [
+                new hwp({
+                    template: "./checkin_app_src/template.html",
+                    publicPath: "/checkin"
+                })
 
-                if(process.env.NODE_ENV==='WEBPACK_DEV_SERVER'){
-                    return '/'
+            ],
+            optimization: {
+                minimize: true,
+                minimizer: [new TerserPlugin({
+                    terserOptions: {
+                        compress: {
+                            drop_console: true
+                        }
+                    }
+                })]
+            },
+            devServer: {
+                open: "chrome",
+                host: 'localhost',
+                port: 9001,
+                compress: false,
+                proxy: {
+                    '/graphql': `http://localhost:${GRAPHQL_PORT_INTERNAL}`
                 }
-                else{
-                    return "/checkin"
-                }
-            })()
-        })
-
-    ],
-    devServer: {
-        open: "chrome",
-        host: 'localhost',
-        port: 9001,
-        compress: false,
-        proxy: {
-            '/graphql': `http://localhost:${GRAPHQL_PORT_INTERNAL}`
+            }
         }
     }
 }
