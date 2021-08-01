5319dc26af69609652e326edd957b80db0a4c0a6
chadrick-kwag <chadrick.kwag@gmail.com>
2021-08-01 15:30:20 +0900

    update instructor resolvers

--- server/resolvers/instructor_resolvers.js
+++ server/resolvers/instructor_resolvers.js
@@ -2,13 +2,22 @@ const pgclient = require('../pgclient')
 const {
     parse_incoming_date_utc_string,
     parse_incoming_gender_str,
-    incoming_time_string_to_postgres_epoch_time
+    incoming_time_string_to_postgres_epoch_time,
+    ensure_admin_account_id_in_context
 } = require('./common')
 
 module.exports = {
     Query: {
 
-        fetch_instructor_stat: async (parent, args) => {
+        fetch_instructor_stat: async (parent, args, context) => {
+
+            if (!ensure_admin_account_id_in_context(context)) {
+                return {
+                    success: false,
+                    msg: 'not admin'
+                }
+            }
+
             try {
                 let result = await pgclient.query(`select count(1) as totalcount from instructor`)
 
@@ -27,75 +36,118 @@ module.exports = {
             }
         },
 
-        fetch_instructors: async () => {
-            let result = await pgclient.query(`select instructor.id as id,name,phonenumber, created, is_apprentice, birthdate, validation_date, memo, job, address, email, gender, level, instructor_level.level_string as level_string, disabled from instructor
-            left join instructor_level on level=instructor_level.id
-            `).then(res => {
+        fetch_instructors: async (parent, args, context) => {
 
-                console.log(res)
-                console.log(res.rows)
+            console.log(context)
+            if (!ensure_admin_account_id_in_context(context)) {
+                return {
+                    success: false,
+                    msg: 'not admin'
+                }
+            }
+
+            try {
+                let result = await pgclient.query(`select instructor.id as id,
+                person.name as name,
+                person.phonenumber as phonenumber, 
+                person.created, is_apprentice, birthdate, validation_date, memo, job, address, person.email, person.gender, level, instructor_level.level_string as level_string, disabled from instructor
+                left join person on person.id = instructor.personid
+                left join instructor_level on level=instructor_level.id
+                `)
 
                 return {
                     success: true,
-                    instructors: res.rows
-
+                    instructors: result.rows
                 }
-            }).catch(e => {
+            }
+            catch (e) {
                 console.log(e)
+
                 return {
-                    success: false,
-                    msg: 'query error'
-
+                    success: false
                 }
-            })
-
-            return result
+            }
         },
 
         search_instructor_with_name: async (parent, args, context, info) => {
-            let results = await pgclient.query("select * from instructor where name=$1", [args.name]).then(res => {
-                return res.rows
-            })
-                .catch(e => [])
-
-            return results
-        },
-
-        fetch_instructor_with_id: async (parent, args) => {
-
-            console.log("inside fetch instructor with id")
-
-            console.log(args)
-
-            let result = await pgclient.query('select instructor.id as id, name,phonenumber, created, is_apprentice, birthdate, validation_date, memo, job, address, email, gender, level, instructor_level.level_string as level_string, disabled, allow_teach_apprentice from instructor left join instructor_level on level = instructor_level.id where instructor.id=$1', [args.id]).then(res => {
-
-                if (res.rowCount == 1) {
-                    return {
-                        success: true,
-                        instructor: res.rows[0]
-                    }
-                }
-                else {
-                    return {
-                        success: false,
-                        msg: "rowcount not 1"
-                    }
-                }
 
 
-            }).catch(e => {
-                console.log(e)
+
+            if (!ensure_admin_account_id_in_context(context)) {
                 return {
                     success: false,
-                    msg: "query error"
+                    msg: 'not admin'
                 }
-            })
+            }
 
-            return result
+
+            try {
+                let results = await pgclient.query(`select instructor.*,
+                person.name,
+                person.phonenumber,
+                person.gender,
+                person.email,
+                person.created
+                 from instructor 
+                left join person on person.id = insturctor.personid
+                where person.name=$1`, [args.name])
+
+                return results.rows
+            }
+            catch (e) {
+                console.log(e)
+
+                return []
+            }
+
         },
-        fetch_instructor_level_info: async (parent, args) => {
-            console.log('fetch_instructor_level_info')
-            console.log(args)
+
+        fetch_instructor_with_id: async (parent, args, context) => {
+
+
+
+            if (!ensure_admin_account_id_in_context(context)) {
+                return {
+                    success: false,
+                    msg: 'not admin'
+                }
+            }
+
+
+
+            try {
+
+                let result = await pgclient.query(`select instructor.id as id, person.name, person.phonenumber, person.created, is_apprentice, birthdate, validation_date, memo, job, address, person.email, person.gender, level, instructor_level.level_string as level_string, disabled, allow_teach_apprentice from instructor 
+                left join instructor_level on level = instructor_level.id 
+                left join person on person.id = instructor.personid
+                where instructor.id=$1`, [args.id])
+
+                return {
+                    success: true,
+                    instructor: result.rows[0]
+                }
+
+            }
+            catch (e) {
+                console.log(e)
+
+                return {
+                    success: false,
+                    msg: e.detail
+                }
+            }
+
+
+        },
+        fetch_instructor_level_info: async (parent, args, context) => {
+
+            if (!ensure_admin_account_id_in_context(context)) {
+                return {
+                    success: false,
+                    msg: 'not admin'
+                }
+            }
+
             try {
                 let result = await pgclient.query(`select id, rank, level_string, active, non_group_lesson_pay_percentage::float, group_lesson_perhour_payment, group_lesson_perhour_penalized_payment from instructor_level`)
 
@@ -115,88 +167,187 @@ module.exports = {
     },
     Mutation: {
 
-        create_instructor: async (parent, args) => {
+        create_instructor: async (parent, args, context) => {
 
-            console.log(args)
+            if (!ensure_admin_account_id_in_context(context)) {
+                return {
+                    success: false,
+                    msg: 'not admin'
+                }
+            }
 
-            let _args = [args.name, args.phonenumber, parse_incoming_gender_str(args.gender), args.email, args.job, parse_incoming_date_utc_string(args.validation_date), args.is_apprentice, args.memo, args.address, parse_incoming_date_utc_string(args.birthdate), args.level]
+            try {
+                // check if exist in person
+                await pgclient.query('begin')
 
-            console.log(_args)
+                let result = await pgclient.query(`select id from person where name=$1 and phonenumber=$2`, [args.name, args.phonenumber])
 
-            let ret = await pgclient.query('insert into instructor (name, phonenumber, gender, email, job, validation_date, is_apprentice, memo, address, birthdate, level) values ($1, $2, $3, $4, $5, to_timestamp($6), $7, $8, $9, to_timestamp($10), $11) ON CONFLICT (name, phonenumber) DO NOTHING', _args).then(res => {
+                let personid
+                if (result.rowCount === 0) {
 
-                console.log(res)
-                if (res.rowCount > 0) return {
+                    let _gender = null
+                    if (args.gender.toLowerCase() === 'male') {
+                        _gender = 'MALE'
+                    }
+                    else if (args.gender.toLowerCase() === 'female') {
+                        _gender = 'FEMALE'
+                    }
+
+                    result = await pgclient.query(`insert into person (name, phonenumber, gender, email) values ($1, $2, $3, $4) returning id`, [args.name, args.phonenumber, _gender, args.email])
+
+                    personid = result.rows[0].id
+
+
+                }
+                else {
+                    personid = result.rows[0].id
+                }
+
+                // create instructor
+                result = await pgclient.query(`insert into instructor(birthdate, address, job, validation_date, level, memo, personid) values ($1, $2, $3, $4, $5, $6, $7)`, [args.birthdate, args.address, args.job, args.validation_date, args.level, args.memo, personid])
+
+                await pgclient.query('commit')
+
+                return {
+                    success: true
+                }
+            }
+            catch (e) {
+                console.log(e)
+
+                try {
+                    await pgclient.query('rollback')
+                }
+                catch (e2) {
+                    return {
+                        success: false,
+                        msg: e.detail
+                    }
+                }
+
+                return {
+                    success: false,
+                    msg: e.detail
+                }
+            }
+
+        },
+        deleteinstructor: async (parent, args, context) => {
+
+
+            if (!ensure_admin_account_id_in_context(context)) {
+                return {
+                    success: false,
+                    msg: 'not admin'
+                }
+            }
+
+
+            try {
+                let result = await pgclient.query('delete from instructor where id=$1', [args.id])
+
+                return {
+                    success: true
+                }
+            }
+            catch (e) {
+                console.log(e)
+
+                return {
+                    success: false,
+                    msg: e.detail
+                }
+            }
+
+        },
+        update_instructor: async (parent, args, context) => {
+
+
+
+            if (!ensure_admin_account_id_in_context(context)) {
+                return {
+                    success: false,
+                    msg: 'not admin'
+                }
+            }
+
+            try {
+
+                await pgclient.query('begin')
+
+                // update instructor info first
+
+                let result = await pgclient.query(`update instructor set level=$1, address=$2, validation_date=$3, birthdate=$4, memo=$5, is_apprentice=$6, job=$7, allow_teach_apprentice=$8 where id=$9 returning personid`, [args.level, args.address, args.validation_date, args.birthdate, args.memo, args.is_apprentice, args.job, args.allow_teach_apprentice, args.id])
+
+
+                const personid = result.rows[0].personid
+
+
+                result = await pgclient.query(`update person set name=$1, phonenumber=$2, gender=$3, email=$4 where id=$5`, [args.name, args.phonenumber, args.gender, args.email, personid])
+
+
+                await pgclient.query('commit')
+
+                return {
                     success: true
                 }
 
-                return {
-                    success: false,
-                    msg: 'query failed. possibly duplicate exist'
-                }
-            }).catch(e => {
+
+
+            } catch (e) {
                 console.log(e)
 
-                return {
-                    success: false,
-                    msg: "query error"
+                try {
+                    await pgclient.query('rollback')
+
                 }
-            })
-
-            return ret
-
-        },
-        deleteinstructor: async (parent, args) => {
-            let ret = await pgclient.query('delete from instructor where id=$1', [args.id]).then(res => {
-                if (res.rowCount > 0) {
-                    return true
-                }
-
-                return false
-            }).catch(e => false)
-
-            return { success: ret }
-        },
-
-
-        update_instructor: async (parent, args) => {
-            console.log('inside update instructor')
-            console.log(args)
-
-            let validation_date = args.validation_date
-            if (validation_date !== null) {
-                validation_date = incoming_time_string_to_postgres_epoch_time(validation_date)
-            }
-
-            let birthdate = args.birthdate
-            if (birthdate !== null) {
-                birthdate = incoming_time_string_to_postgres_epoch_time(birthdate)
-            }
-
-            let _args = [args.name, args.phonenumber, parse_incoming_gender_str(args.gender), args.email, args.level, args.address, validation_date, birthdate, args.memo, args.is_apprentice, args.job, args.allow_teach_apprentice, args.id]
-
-            console.log(_args)
-
-            let ret = await pgclient.query('update instructor set name=$1, phonenumber=$2, gender=$3, email=$4, level=$5, address=$6, validation_date=to_timestamp($7), birthdate=to_timestamp($8), memo=$9, is_apprentice=$10, job=$11, allow_teach_apprentice=$12 where id=$13', _args).then(res => {
-                if (res.rowCount > 0) {
+                catch (e2) {
                     return {
-                        success: true
+                        success: false,
+                        msg: e.detail
                     }
                 }
+
                 return {
                     success: false,
-                    msg: 'no instructor updated'
+                    msg: e.detail
                 }
-            }).catch(e => {
-                console.log(e)
-                return {
-                    success: true,
-                    msg: 'error updating instructor'
-                }
-            })
+            }
+
+            // let validation_date = args.validation_date
+            // if (validation_date !== null) {
+            //     validation_date = incoming_time_string_to_postgres_epoch_time(validation_date)
+            // }
+
+            // let birthdate = args.birthdate
+            // if (birthdate !== null) {
+            //     birthdate = incoming_time_string_to_postgres_epoch_time(birthdate)
+            // }
+
+            // let _args = [args.name, args.phonenumber, parse_incoming_gender_str(args.gender), args.email, args.level, args.address, validation_date, birthdate, args.memo, args.is_apprentice, args.job, args.allow_teach_apprentice, args.id]
+
+            // console.log(_args)
+
+            // let ret = await pgclient.query('update instructor set name=$1, phonenumber=$2, gender=$3, email=$4, level=$5, address=$6, validation_date=to_timestamp($7), birthdate=to_timestamp($8), memo=$9, is_apprentice=$10, job=$11, allow_teach_apprentice=$12 where id=$13', _args).then(res => {
+            //     if (res.rowCount > 0) {
+            //         return {
+            //             success: true
+            //         }
+            //     }
+            //     return {
+            //         success: false,
+            //         msg: 'no instructor updated'
+            //     }
+            // }).catch(e => {
+            //     console.log(e)
+            //     return {
+            //         success: true,
+            //         msg: 'error updating instructor'
+            //     }
+            // })
 
 
-            return ret
+            // return ret
         },
         disable_instructor_by_id: async (parent, args) => {
             let result = await pgclient.query('update instructor set disabled=true where id=$1', [args.id]).then(res => {
--- server/typedefs/instructor_typedefs.js
+++ server/typedefs/instructor_typedefs.js
@@ -44,7 +44,7 @@ type SuccessResultWithId{
 
 type Mutation {
     
-    create_instructor(name: String!, phonenumber: String!, email: String, job: String, validation_date: String, memo: String, address: String, birthdate: String, is_apprentice: Boolean, level: String, gender: String): SuccessResult
+    create_instructor(name: String!, phonenumber: String!, email: String, job: String, validation_date: String, memo: String, address: String, birthdate: String, is_apprentice: Boolean, level: Int, gender: String): SuccessResult
       
     deleteinstructor(id: Int!): SuccessResult
     
